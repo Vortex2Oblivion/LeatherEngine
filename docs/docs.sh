49a10c849099a52cbc60d55f38d9b2b1c8bd15dd
Lily <egzozu.be.bas@gmail.com>
2024-06-25 11:46:25 +0300

    guh

--- docs/docs.sh
+++ docs/docs.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 haxe docs/docs.hxml
-haxelib run dox -i docs -o pages --title "Leather Engine Mobile Documentation" -in "mobile" -in "android.utilities.LeatherJNI"
+haxelib run dox -i docs -o pages --title "Leather Engine Mobile Documentation" -in "mobile" -in "android"
