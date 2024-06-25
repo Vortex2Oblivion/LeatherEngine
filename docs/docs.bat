12c4d65b6c80512ce9ead1d17d9c784033eda4de
Lily <egzozu.be.bas@gmail.com>
2024-06-25 14:06:42 +0300

    view source thing

--- docs/docs.bat
+++ docs/docs.bat
@@ -1,3 +1,3 @@
 
 haxe docs/docs.hxml
-haxelib run dox -i docs -o pages --title "Leather Engine Mobile Documentation" -in "mobile" -in "android"
+haxelib run dox -i docs -o pages --title "Leather Engine Mobile Documentation" -in "mobile" -in "android" -D source-path "https://github.com/MobilePorting/LeatherEngine-LTS-Mobile/tree/main/source"
--- docs/docs.sh
+++ docs/docs.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 haxe docs/docs.hxml
-haxelib run dox -i docs -o pages --title "Leather Engine Mobile Documentation" -in "mobile" -in "android"
+haxelib run dox -i docs -o pages --title "Leather Engine Mobile Documentation" -in "mobile" -in "android" -D source-path "https://github.com/MobilePorting/LeatherEngine-LTS-Mobile/tree/main/source"
